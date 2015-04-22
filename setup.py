7c53840c45e2d0592520b4425fb0949eaa10e203
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2015-04-22 18:10:39 +0900

    update setup.py for registering pypi

--- setup.py
+++ setup.py
@@ -3,16 +3,29 @@
 import sys
 from setuptools import setup, find_packages
 
+
 sys.path.append('./src')
 sys.path.append('./test')
 
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 2.7",
+    "Intended Audience :: Developers",
+    "Topic :: Scientific/Engineering :: GIS",
+]
 
-setup(name='py-geohex3',
-      version='0.0.1',
-      description='geohex v3 python implementation',
-      author='UNCOVER TRUTH Inc.',
-      author_email='info@uncovertruth.jp',
-      test_suite = 'geohex_test.suite',
-      packages=find_packages("src"),
-      package_dir={"": "src"})
-
+setup(
+    name='py-geohex3',
+    description='geohex v3.2 python implementation',
+    url='https://github.com/uncovertruth/py-geohex3',
+    version='0.0.1',
+    author='UNCOVER TRUTH Inc.',
+    author_email='info@uncovertruth.jp',
+    test_suite='geohex_test.suite',
+    classifiers=classifiers,
+    platforms='any',
+    keywords='gis geohex',
+    packages=find_packages("src"),
+    package_dir={"": "src"}
+)
